e8c6bd8bed1d185850183e330b3fc77c2fc4e9df
User Name <artur.it2914@gmail.com>
2020-06-16 19:24:15 +0300

    Add String / other types examples

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-console.log('\'A\' + true = ', 'A ' + true );
-console.log('\'A\' + false = ', 'A ' + false );
-console.log('\'A\' + undefined = ', 'A ' + undefined );
-console.log('\'A\' + null = ', 'A ' + null );
-console.log('\'A\' + 987 = ', 'A ' + 987 );
-console.log('\'A\' + {} = ', 'A ' + {} );
-console.log('\'A\' + Symbol() = ', 'A ' + Symbol() );
+console.group( ' - ' );
+console.log( '7 - true = ', '7' - true );
+console.log( '7 - false = ', '7' - false );
+console.log( '7 - undefined = ', '7' - undefined );
+console.log( '7 - null = ', '7' - null );
+console.log( '7 - 987 = ', '7' - 987 );
+console.log( '7 - {} = ', '7' - {} );
+console.groupEnd();
+
+console.group( ' / ' );
+
+console.log( '7 / true = ', '7' / true );
+console.log( '7 / false = ', '7' / false );
+console.log( '7 / undefined = ', '7' / undefined );
+console.log( '7 / null = ', '7' / null );
+console.log( '7 / 987 = ', '7' / 987 );
+console.log( '7 / {} = ', '7' / {} );
+console.log( '7 / Symbol() = ', '7' / Symbol() );
+console.groupEnd();
