42ead01c160f8f6d473af9f74ff54e33723c1938
User Name <artur.it2914@gmail.com>
2020-06-18 19:52:31 +0300

    Add constructors examples

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
-console.group( ' - ' );
-console.log( '7 - true = ', '7' - true );
-console.log( '7 - false = ', '7' - false );
-console.log( '7 - undefined = ', '7' - undefined );
-console.log( '7 - null = ', '7' - null );
-console.log( '7 - 987 = ', '7' - 987 );
-console.log( '7 - {} = ', '7' - {} );
-console.groupEnd();
+const user = {
+  name: 'Test',
+};
+console.log( user );
 
-console.group( ' / ' );
+user.isMale = true;
+console.log( user );
+user.age = 18;
+console.log( user );
+user.speak = function () {
+  alert( 'I\'m speaking' );
+};
+console.log( user );
+user.name = 'Ivan';
+console.log( user );
 
-console.log( '7 / true = ', '7' / true );
-console.log( '7 / false = ', '7' / false );
-console.log( '7 / undefined = ', '7' / undefined );
-console.log( '7 / null = ', '7' / null );
-console.log( '7 / 987 = ', '7' / 987 );
-console.log( '7 / {} = ', '7' / {} );
-console.log( '7 / Symbol() = ', '7' / Symbol() );
-console.groupEnd();
+/**
+ *
+ * @param {string} name
+ * @param {boolean} isMale
+ * @constructor
+ * */
+function Cat(name, isMale) {
+  this.name = name;
+  this.isMale = isMale;
+
+}
+
+const cat1 = new Cat( 'Murzik', true );
+const cat2 = new Cat( 'Dog', true );
+const cat3 = new Cat( 'Cake', false );
+
+/**
+ *
+ * @param firstName
+ * @param lastName
+ * @param age
+ * @constructor
+ */
+function User(firstName, lastName, age) {
+  this.name = firstName;
+  this.surname = lastName;
+  this.age = age;
+}
+
+const user1 = new User( 'Ivan', 'Ivanov', 16 );
+const user2 = new User( 'Test', 'Testovich', 95 );
+
+
+
