c70017e06e9d8533fda57e239e8e2a79101025dd
User Name <artur.it2914@gmail.com>
2020-06-23 18:49:51 +0300

    Add do while cycle example

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-const firstValue = prompt( 'Please input you first number' );
-const secondValue = prompt( 'Please input you second number' );
+const CORRECT_PASSWORD = 'qwerty';
+const TRY_LIMIT = 5;
 
-alert( `${firstValue} + ${secondValue} = ${Number( firstValue ) + Number( secondValue )}` );
+let tryCount = 0;
+let isPasswordCorrect = false;
+
+do {
+
+  isPasswordCorrect = prompt( 'Please input your password' ) === CORRECT_PASSWORD;
+
+  if (isPasswordCorrect) {
+    break;
+  }
+
+} while (++tryCount < TRY_LIMIT);
+
+alert( `Password ${isPasswordCorrect
+                   ? 'incorrect'
+                   : 'correct'}` );
 
 
