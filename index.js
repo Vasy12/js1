a576ec4e5cb395b7a4cf2e101509a879644e0aa4
User Name <artur.it2914@gmail.com>
2020-06-23 20:48:05 +0300

    Add getNthFibNumber function

--- index.js
+++ index.js
@@ -8,18 +8,18 @@
 /**
  *
  * @param {number} base
- * @param {number} power - positive value
+ * @param {number} exponent
  * @return {number}
  */
-function pow(base, power) {
+function pow(base, exponent) {
 
-  if (power < 0) {
-    return 1 / pow( base, Math.abs( power ) );
+  if (exponent < 0) {
+    return 1 / pow( base, Math.abs( exponent ) );
   }
 
   let result = 1;
 
-  for (let i = 0; i < power; i++) {
+  for (let i = 0; i < exponent; i++) {
     result *= base;
   }
 
@@ -29,3 +29,31 @@ function pow(base, power) {
 
 console.log( `pow( 2, -3 ) = ${pow( 2, -3 )}` );
 
+/**
+ *
+ * @param {number} n
+ * @return {number}
+ */
+function getNthFibNumber(n) {
+
+  if (n === 1) {
+    return 0;
+  }
+
+  let prev1 = 0;
+  let prev2 = 1;
+  let next = prev1 + prev2;
+
+  for (let i = 3; i < n; i++) {
+    prev1 = prev2;
+    prev2 = next;
+    next = prev1 + prev2;
+  }
+
+  return next;
+}
+
+console.log( `getNthFibNumber( 7 ) = ${getNthFibNumber( 7 )}` ); // 8
+console.log( `getNthFibNumber( 9 ) = ${getNthFibNumber( 9 )}` ); // 21
+
+console.log( 2 ** 3 );
