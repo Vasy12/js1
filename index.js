b72d8a32622507a72ccb6604ceed9113b2e78e2b
User Name <artur.it2914@gmail.com>
2020-06-24 18:02:19 +0300

    Add multiplicity solution

--- index.js
+++ index.js
@@ -1,59 +1,28 @@
-/*let i = 0;
+/*
+ * Вывести все числа от 1 до 100, которые кратные указанному
+ пользователем числу.
 
- while (i <= 10) {
-
- console.log( i++ );
- }*/
-
-/**
  *
- * @param {number} base
- * @param {number} exponent
- * @return {number}
- */
-function pow(base, exponent) {
+ * */
 
-  if (exponent < 0) {
-    return 1 / pow( base, Math.abs( exponent ) );
+const userNumber = Number( prompt() );
+
+if (!isNaN( userNumber )) {
+  console.group( 'Not optimal solution' );
+
+  for (let i = 0; i < 100; i++) {
+    if (i % userNumber === 0) {
+      console.log( i );
+    }
   }
+  console.groupEnd();
 
-  let result = 1;
+  console.group( 'Optimal solution' );
 
-  for (let i = 0; i < exponent; i++) {
-    result *= base;
+  for (let i = 0; i < 100; i += userNumber) {
+    console.log( i );
   }
-
-  return result;
+  console.groupEnd();
 
 }
 
-console.log( `pow( 2, -3 ) = ${pow( 2, -3 )}` );
-
-/**
- *
- * @param {number} n
- * @return {number}
- */
-function getNthFibNumber(n) {
-
-  if (n === 1) {
-    return 0;
-  }
-
-  let prev1 = 0;
-  let prev2 = 1;
-  let next = prev1 + prev2;
-
-  for (let i = 3; i < n; i++) {
-    prev1 = prev2;
-    prev2 = next;
-    next = prev1 + prev2;
-  }
-
-  return next;
-}
-
-console.log( `getNthFibNumber( 7 ) = ${getNthFibNumber( 7 )}` ); // 8
-console.log( `getNthFibNumber( 9 ) = ${getNthFibNumber( 9 )}` ); // 21
-
-console.log( 2 ** 3 );
